da1ea7f20da3dc3e3a8ea770de4acb71274cbf5f
ridhi-2801 <ridhi2801@gmail.com>
2021-05-28 01:33:57 +0530

    category_page.dart

--- lib/category_page.dart
+++ lib/category_page.dart
@@ -1,27 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/LinkCards.dart';
+import 'package:flutter_app/constants.dart';
 
 class CategoryPage extends StatelessWidget {
   final categoryName;
-
+  final fireStore = FirebaseFirestore.instance;
   CategoryPage({this.categoryName});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: isDark?darkModeColor:baseColor,
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.transparent,
-        title: Text(categoryName, style: TextStyle(fontFamily: 'BalsamiqSans'),),
+        backgroundColor: isDark?darkModeColor:baseColor,
+        title: Text(categoryName, style: TextStyle(fontFamily: 'BalsamiqSans',fontSize:30,color:isDark?baseColor:darkModeColor),),
         leading: IconButton(
-          icon: Icon(EvaIcons.arrowIosBack),
+          icon: Icon(EvaIcons.arrowIosBack,color:isDark?baseColor:darkModeColor),
           onPressed: (){
             Navigator.pop(context);
           },
         ),
       ),
-      body: Column(
+      body: Padding(
+        padding: const EdgeInsets.only(top:18.0),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+     children: [
+         Row(
+           mainAxisAlignment: MainAxisAlignment.spaceAround,
+           children: [
+             LinkCards(groupImage: "", groupNameText: "", linkText: ""),
+             LinkCards(groupImage: "", groupNameText: "", linkText: ""),
+           ],
+         ),
+     ],
 
+
+        ),
       ),
     );
   }
