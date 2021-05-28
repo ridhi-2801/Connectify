351d7d2aa25191cc303d52758b41effac4776ec2
kaabil <aamir.zaidi5@gmail.com>
2021-05-28 17:12:50 +0530

    commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
 final Color baseColor = Colors.white;
 final Color darkModeColor = Colors.black;
 bool isDark=false;
+
 launchURL(String url) async {
   if (await canLaunch(url)) {
     await launch(url);
@@ -16,3 +18,14 @@ launchURL(String url) async {
 
 final textStyle=TextStyle(fontSize: 20,fontWeight: FontWeight.w500);
 final loginColor=Color(0xff744cbc);
+
+
+
+var iconMap = {
+  'Coding':EvaIcons.smartphoneOutline,
+  'Fashion':EvaIcons.shoppingBagOutline,
+  'Photography':EvaIcons.cameraOutline,
+  'Google' :EvaIcons.google,
+  'Github' :EvaIcons.githubOutline,
+  'Linkedin': EvaIcons.linkedinOutline,
+};
