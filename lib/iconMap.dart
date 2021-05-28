0cdbc5659413e04bf016076eece7162378fa1fbd
ridhi-2801 <ridhi2801@gmail.com>
2021-05-28 16:31:27 +0530

    iconMap

---
+++ lib/iconMap.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+
+var iconMap = {
+  'tech':EvaIcons.smartphoneOutline,
+  'fashion':EvaIcons.shoppingBagOutline,
+  'photography':EvaIcons.cameraOutline,
+   'Google' :EvaIcons.google,
+   'Github' :EvaIcons.githubOutline,
+   'Linkedin': EvaIcons.linkedinOutline,
+   'Tech': EvaIcons.smartphoneOutline,
+   'Fashion': EvaIcons.shoppingBagOutline,
+   'Photography': EvaIcons.cameraOutline,
+
+};
